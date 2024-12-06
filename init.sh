2d40d224f2601e3a4cff6b676536765c0a8a014d
Andrew Berry <andrew@furrypaws.ca>
2024-12-06 14:15:51 -0500

    Add a fresh clone script

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/Koenkk/zigbee-herdsman
+git clone https://github.com/Koenkk/zigbee-herdsman-converters
+git clone https://github.com/Koenkk/zigbee2mqtt
+git clone https://github.com/nurikk/zigbee2mqtt-frontend.git
