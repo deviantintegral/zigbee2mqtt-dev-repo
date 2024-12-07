f25479ca524f3ab4d4f08f519f01ddc186fabba0
Andrew Berry <andrew@furrypaws.ca>
2024-12-06 20:47:26 -0500

    We need the dev branch

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/Koenkk/zigbee-herdsman
 git clone https://github.com/Koenkk/zigbee-herdsman-converters
-git clone https://github.com/Koenkk/zigbee2mqtt
+git clone --branch dev https://github.com/Koenkk/zigbee2mqtt
 git clone https://github.com/nurikk/zigbee2mqtt-frontend.git
